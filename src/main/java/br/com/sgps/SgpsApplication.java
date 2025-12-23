4eeb7c6652db5aa25ddff398e0e047f4d256f671
ismael.francisco <isma@99571>
2025-12-23 11:32:29 -0300

    versao inicial projeto

---
+++ src/main/java/br/com/sgps/SgpsApplication.java
@@ -0,0 +1,13 @@
+package br.com.sgps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sgps/SgpsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sgps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
